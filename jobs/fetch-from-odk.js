a3eeb4e68b53f647e4690dee77bc51efd8a9909a
Taylor Downs <taylor@openfn.org>
2021-11-30 07:54:09 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/fetch-from-odk.js
+++ jobs/fetch-from-odk.js
@@ -1,4 +1,11 @@
-// language-http
-
-get('someUrl', { ...params }, callback);
-post('someUrl', { body: state => state.data })'
+get(
+  "/v1/projects/6/forms/CAMFED_Annual_Data_2021.svc/Submissions", // the ODK central endpoint
+  {
+    filter: 'year(__system%2FsubmissionDate)%20lt%20year(now())', // before now?
+    expand: '&#42' // expand all references?
+  }, // extra options
+  state => { // callback to show off some fancy stuff
+    console.log(JSON.stringify(state.data, null, 2));
+    return { ...state, references: [], submissions: state.data.value };
+  }
+ );
