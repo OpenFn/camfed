073069878d4f5272429a73338052037990bf0b4d
Taylor Downs <taylor@openfn.org>
2021-11-30 07:55:00 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/upsert-in-salesforce.js
+++ jobs/upsert-in-salesforce.js
@@ -1,1 +1,9 @@
-// tbd
+each('$.submissions[*]',
+  upsert('Camfed_Annual_data__c', 'MetaInstanceID__c',
+    fields(
+      field('School__c', dataValue('School__c')),
+      field('MetaInstanceID__c', dataValue('meta.instanceID')),
+      // ...Taylor to add some more fields once SF privs are confirmed
+    )
+  )
+);
