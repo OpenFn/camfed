68cf6179abe1ed89356454d99fd2c2e14c9a1fd9
Taylor Downs <downs.taylor@gmail.com>
2021-11-30 08:35:24 +0000

    provide additional examples

--- jobs/upsert-in-salesforce.js
+++ jobs/upsert-in-salesforce.js
@@ -1,9 +1,30 @@
-each('$.submissions[*]',
-  upsert('Camfed_Annual_data__c', 'MetaInstanceID__c',
+each(
+  '$.submissions[*]',
+  upsert(
+    'Camfed_Annual_data__c',
+    'MetaInstanceID__c',
     fields(
       field('School__c', dataValue('School__c')),
       field('MetaInstanceID__c', dataValue('meta.instanceID')),
-      // ...Taylor to add some more fields once SF privs are confirmed
+      field('Female_Enrolment__c', dataValue('Total_Female_Enrolment')),
+      field('Male_Enrolment__c', dataValue('Total_Male_Enrolment')),
+      field(
+        'TM_in_Place__c',
+        dataValue('TM_in_Place__c') == 'Yes' ? true : false
+      )
     )
   )
 );
+
+// Note: At any time, you can also use a function which takes state...
+// field('TM_in_Place__c', state => {
+//   switch (state.data.TM_in_Place__c) {
+//     case 'Yes':
+//     case 'Yeah':
+//     case 'Definitely':
+//       return true;
+//
+//     default:
+//       return false;
+//   }
+// })
